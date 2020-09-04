2538aa7fec3620e136df27681adbdaa1f680686e
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-09-04 17:56:40 +0200

    create multiply by index method array_methods.rb

--- rb/methods/array_methods.rb
+++ rb/methods/array_methods.rb
@@ -38,3 +38,24 @@ end
 
 yell(["hello", "world"])
 yell(["code", "is", "cool"])
+
+# Write a method element_times_index(nums) that takes in an array of
+# numbers and returns a new array containing every number of the
+# original array multiplied with its index.
+
+def element_times_index(numbers)
+	multi = []
+  
+	i = 0
+	while i < numbers.length
+	item = numbers[i]*i
+	multi << item
+	i += 1
+    end
+
+	return multi
+end
+
+print element_times_index([4, 7, 6, 5])       # => [0, 7, 12, 15]
+puts
+print element_times_index([1, 1, 1, 1, 1, 1]) # => [0, 1, 2, 3, 4, 5]
