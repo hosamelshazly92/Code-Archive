b4c444e30d7f778d4afe657ccef2e41888f12638
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-09-04 12:59:07 +0200

    create print method print.rb

--- rb/demo.rb
+++ rb/demo.rb
@@ -1,3 +1,5 @@
+# print statements
+
 puts "Welcome sir" 
 
 print "\t Hosam\n"
@@ -6,4 +8,3 @@ print "Elshazly\n"
 p "4"
 p 4
 
-p true
---
+++ rb/methods/print.rb
@@ -0,0 +1,9 @@
+# print a message a number of times
+
+def sayMessage
+	puts "access granted"
+end
+
+sayMessage
+sayMessage
+sayMessage
