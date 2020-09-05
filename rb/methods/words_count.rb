dfc23c5f7f6f4b432410ce8244d66d42fc6bb58a
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-09-05 09:39:54 +0200

    add comment words_count.rb

--- rb/methods/words_count.rb
+++ rb/methods/words_count.rb
@@ -1,10 +1,10 @@
 # Write a method element_count that takes in an array and returns a hash
 # representing the count of each element in the array.
 
-def element_count(arr)
+def word_count(arr)
     count = Hash.new(0)
     arr.each { |i| count[i] += 1 }
     return count
 end
 
-element_count(["red", "red", "blue", "green"])
+word_count(["red", "red", "blue", "green"])
