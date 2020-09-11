dd364e6c14a2f33c105804a498e1fa7c85fd00ed
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-09-11 20:02:57 +0200

    create frame

--- py/gui/message.py
+++ py/gui/message.py
@@ -1,18 +1,23 @@
 import tkinter as tk
 
-HGT = 400
 WDH = 600
+HGT = 400
 
 # create window via root
 root = tk.Tk()
 
 # create container
-canvas = tk.Canvas(root, height=HGT, width=WDH)
+canvas = tk.Canvas(root, width=WDH, height=HGT)
 # place canvas
 canvas.pack()
 
+# create frame
+frame = tk.Frame(root, bg="white")
+# place frame
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
 # create button
-button = tk.Button(root, text="OK", bg="green", fg="white")
+button = tk.Button(frame, text="OK", bg="green", fg="white")
 # place button
 button.pack()
 
