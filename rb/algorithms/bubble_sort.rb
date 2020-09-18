4ff867fc8a7be8fe2ed36bade561c500513d104c
hosam <hosamelshazly92@gmail.com>
2020-09-18 16:06:46 +0200

    perform sorting for one pass bubble_sort.rb

--- rb/algorithms/bubble_sort.rb
+++ rb/algorithms/bubble_sort.rb
@@ -1,5 +1,13 @@
+# buuble sort algorithm
 def bubble_sort(arr)
 
+    (0...arr.length - 1).each do |i|
+        if arr[i] > arr[i + 1]
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        end
+    end
+
+    arr
 end
 
 # test case
