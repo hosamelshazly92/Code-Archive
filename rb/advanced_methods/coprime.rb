6e259c885b7bc543814dd92db61e24d2ef2bf4a2
hosam <hosamelshazly92@gmail.com>
2020-09-19 16:34:07 +0200

    edit coprime.rb advanced methods

--- rb/advanced_methods/coprime.rb
+++ rb/advanced_methods/coprime.rb
@@ -7,7 +7,8 @@ def coprime?(num_1, num_2)
     (2..num_1).none? { |factor| num_1 % factor == 0 && num_2 % factor == 0}
 end
 
-p coprime?(25, 12)    # => true
-p coprime?(7, 11)     # => true
-p coprime?(30, 9)     # => false
-p coprime?(6, 24)     # => false
+# test case
+coprime?(25, 12)
+coprime?(7, 11)
+coprime?(30, 9)
+coprime?(6, 24)
